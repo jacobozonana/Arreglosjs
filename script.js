e5533f3f2f9d415dcbb9766d2107edeab2d068ac
Jacobo Zonana <jacobozonana@gmail.com>
2020-08-08 22:38:26 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var platillo = ["Enchiladas","Tacos","Tamal","Pozole","Barbacoa","Picada"];
+
+
+//Ejercicio 1
+// Mediante un ciclo FOR, muestra todos los platillos (recuerda la propiedad length)
+
+
+
+//for (var i = 0; i <platillo.length;i++){
+	//console.log(platillo[i])
+//}
+
+//Ejercicio 2
+// Mediante ciclo FOR y condicional IF, muestra solo el pozole y la barbacoa.
+
+//for (var i = 0; i < platillo.length ; i++){
+	//if (platillo[i] == "Pozole" || platillo[i] == "Barbacoa"){
+		//console.log(platillo [i])
+	//}
+//}
+
+
+//Ejercicio 3
+// Mediante ciclo FOR y condicional IF, muestra solo los elementos impares.
+
+
+//for (var i = 0; i <= platillo.length; i++){
+	//if (i % 2 !=0){
+		//console.log("A mi me gusta el " + platillo[i])
+		//}
+//}
+
+//Ejercicio 4
+// Mediante únicamente un ciclo FOR, muestra los elementos que sean pares.
+
+//for (var i = 0; i<platillo.length; i+=2){
+	//	console.log(platillo[i])
+//}
+
+
+
